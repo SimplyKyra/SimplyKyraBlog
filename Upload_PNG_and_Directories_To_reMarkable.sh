b9ab41206e208b3f7c8a8a3918c13da4448105f5
SimplyKyra <SimplyKyra@users.noreply.github.com>
2021-03-05 13:28:52 -0800

    Edited the comments
    I made the comments a bit more refined.

--- Upload_PNG_and_Directories_To_reMarkable.sh
+++ Upload_PNG_and_Directories_To_reMarkable.sh
@@ -2,8 +2,8 @@
 
 ##### Variables Used #####
 
-# How do you connect to you're reMarkable. I use a shortcut so I do "ssh remarkable". Another way might be "ssh root@IPaddress"
-# Leave off the ssh so in my case it would just be "remarkable". 
+# How do you connect to your reMarkable? I created a shortcut so I connect with "ssh remarkable". Another way might be 
+# with "ssh root@IPaddress". Here I left off the "ssh" so it's just "remarkable". In the second case it would be "root@IPaddress"
 declare sshReMarkable="remarkable"
 
 # Name of the temporary tar ball that's created
@@ -11,16 +11,17 @@ declare archiveName="archivedTemplates.tgz"
 
 ##### Error Checking #####
 
-# Checks if there's any directories in the current directory and saves them for here and further down. 
+# Checks if there are any directories within the current directory and saves them for here and further down. 
 declare currDirectories=$(find . -maxdepth 1 -mindepth 1 -type d)
-# If there aren't any directories it issues a message and exits the program
+# If there aren't any directories that would be a problem so it issues a message and exits the program
 if [ -z "$currDirectories" ]; then
-  echo "Any PNG files needs to be within a directory. This directory would show what category the image belongs to. There are no directories in your current directory. Exiting the program."
+  echo "Any PNG files to be uploaded needs to be within a directory. This directory shows which category the image belongs to. As there are no directories within your current directory the script is exiting now."
   exit 1
 fi
 
-# Saves any nested directories and if they exist issues a warning and asks if the user wants to continue or exit. 
+# Saves any nested directories.
 declare nestedDirectories=$(find . -mindepth 2 -type d)
+# If those nested directories exist a warning is issued and the user choose whether the script should continue or exit. 
 if [ -n "$nestedDirectories" ]; then
   echo "This script assumes that all images are within a single directory. It found the following nested directories:"
   echo "$nestedDirectories"
@@ -42,20 +43,21 @@ for f in $currDirectories; do
   directoryString="$directoryString -name $(basename $f) -o"
 done
 
+# Creates, informs, and executes a command to compress the directories and their image files. 
 declare compressCommand="find . -maxdepth 1 -mindepth 1 -type d \(${directoryString%??}\) -exec tar cfz $archiveName {} +"
 echo "Compressing the directories and files within using this command: $compressCommand"
 eval "$compressCommand"
 
-# Copies the created tarball to the remarkable. 
+# Uploads the created tarball to the reMarkable's templates directory. 
 eval "scp $archiveName $sshReMarkable:/usr/share/remarkable/templates/"
 
 # Creating a command that will ssh into the reMarkable, navigate to the templates folder, extract the tarball, and remove it. 
-# In case it doesn't perform correctly I output it to you and then execute it.
+# In case it doesn't perform correctly I output the command to you before executing it.
 declare executeSSHcommands="ssh $sshReMarkable \"cd /usr/share/remarkable/templates/ && tar -xf $archiveName && rm $archiveName\""
 echo "About to execute $executeSSHcommands"
 eval "$executeSSHcommands"
 
-# Removes the tarball on my device as it's no longer needed.
+# Removes the tarball within the current directory as it's no longer needed.
 rm $archiveName
 
 ##### Generate the JSON snippet #####
@@ -65,16 +67,16 @@ declare jsonString=""
 
 # Uses find to find any PNG images within the current directory and any 
 # sub-directories. It then uses each filename (within the while statement) 
-# to generated the needed templates.json code snippet.i
+# to generated the needed templates.json code snippet.
 for f in $(find . -iname "*.PNG" | sort); do
   jsonString="$jsonString,\n    {\n"
-  # Creates a variable and saves the name, taken from the filename, to the variable. Used in the next line
-  # This is the way I create the name if you use full words with Capitals to separate. Ex. BalancedLifeResetJournal5 --> Balanced Life Reset Journal 5
-  #declare name=$((basename $f | cut -f 1 -d '.' | sed -e 's|\([A-Z0-9][^A-Z0-9]\)| \1|g' -e 's|\([a-z]\)\([A-Z0-9]\)|\1 \2|g') | sed -e 's/^[[:space:]]*//');
-  # Instead of above I went simple and it splits the name on the - and adds a space
+  # Creates a variable and saves the name, taken from the filename, to a variable. There are two ways: 
+  # This is the way I did it before when I wanted full names for the templates. It splits the name based on capital letters while taking into account some instances like FAQ. 
+  #    Example: "BalancedLifeResetJournal5" would become "Balanced Life Reset Journal 5"
+  # declare name=$((basename $f | cut -f 1 -d '.' | sed -e 's|\([A-Z0-9][^A-Z0-9]\)| \1|g' -e 's|\([a-z]\)\([A-Z0-9]\)|\1 \2|g') | sed -e 's/^[[:space:]]*//');
+  # I chose to shorten my names and went with abbreviations. Since that would make it more difficult I now simply split the name by replacing the dash (-) with a space
   declare name=$(basename $f | cut -f 1 -d '.' | sed 's,\-, ,g')
   jsonString="$jsonString\n      \"name\": \"${name//_}\","
-  # basename removes the pathway so you're left with the filename and extension. After the | it removes the stuff after the period so you only have the filename
   # Takes the last folder before the filename as the parent directory.
   declare directoryName=$(basename $(dirname $f));
   jsonString="$jsonString\n      \"filename\": \"$directoryName/$(basename $f  | cut -f 1 -d '.')\","
@@ -86,8 +88,7 @@ for f in $(find . -iname "*.PNG" | sort); do
   jsonString="$jsonString\n    }"
 done
 
-# Outputs the text line to the text editor so you can use the snippet at your leisure
-# Also sends it to your clipboard so you can paste it in right away without opening the file. 
-# Use -e so it sees the new line characters and encircle $jsonString in quotes so it sees the spaces. Ignores the tab otherwise. 
+# Outputs the text to a new file named "myJSONsnippet.txt so you can use the JSON snippet at your leisure. 
 echo -e "$jsonString" > myJSONsnippet.txt
+# Also sends the snippet to your clipboard so you can paste it in right away without needing to open the file.
 echo -e "$jsonString" | pbcopy
